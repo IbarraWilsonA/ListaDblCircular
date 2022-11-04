ed46ae8e6893519d6957300ad4ecfcc7c2fa7e1e
PC-Solution 4-5-22 <PC-Solution 4-5-22@DESKTOP-H0OU5D3>
2022-11-03 22:44:19 -0400

    Agregar archivos de proyecto.

---
+++ ListaDblCircular/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace ListaDblCircular
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnUltimo = new System.Windows.Forms.Button();
+            this.btnAnterior = new System.Windows.Forms.Button();
+            this.btnSiguiente = new System.Windows.Forms.Button();
+            this.btnPrimero = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnInsertar = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtCantidadHoras = new System.Windows.Forms.TextBox();
+            this.txtNombreAsig = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Lime;
+            this.panel2.Controls.Add(this.btnUltimo);
+            this.panel2.Controls.Add(this.btnAnterior);
+            this.panel2.Controls.Add(this.btnSiguiente);
+            this.panel2.Controls.Add(this.btnPrimero);
+            this.panel2.Controls.Add(this.btnEliminar);
+            this.panel2.Controls.Add(this.btnInsertar);
+            this.panel2.Location = new System.Drawing.Point(76, 252);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(495, 148);
+            this.panel2.TabIndex = 25;
+            // 
+            // btnUltimo
+            // 
+            this.btnUltimo.Font = new System.Drawing.Font("Cambria", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUltimo.Location = new System.Drawing.Point(365, 73);
+            this.btnUltimo.Name = "btnUltimo";
+            this.btnUltimo.Size = new System.Drawing.Size(101, 34);
+            this.btnUltimo.TabIndex = 12;
+            this.btnUltimo.Text = "Ultimo";
+            this.btnUltimo.UseVisualStyleBackColor = true;
+            this.btnUltimo.Click += new System.EventHandler(this.btnUltimo_Click);
+            // 
+            // btnAnterior
+            // 
+            this.btnAnterior.Font = new System.Drawing.Font("Cambria", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAnterior.Location = new System.Drawing.Point(260, 73);
+            this.btnAnterior.Name = "btnAnterior";
+            this.btnAnterior.Size = new System.Drawing.Size(99, 34);
+            this.btnAnterior.TabIndex = 11;
+            this.btnAnterior.Text = "Anterior";
+            this.btnAnterior.UseVisualStyleBackColor = true;
+            this.btnAnterior.Click += new System.EventHandler(this.btnAnterior_Click);
+            // 
+            // btnSiguiente
+            // 
+            this.btnSiguiente.Font = new System.Drawing.Font("Cambria", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSiguiente.Location = new System.Drawing.Point(136, 73);
+            this.btnSiguiente.Name = "btnSiguiente";
+            this.btnSiguiente.Size = new System.Drawing.Size(118, 34);
+            this.btnSiguiente.TabIndex = 10;
+            this.btnSiguiente.Text = "Siguiente";
+            this.btnSiguiente.UseVisualStyleBackColor = true;
+            this.btnSiguiente.Click += new System.EventHandler(this.btnSiguiente_Click);
+            // 
+            // btnPrimero
+            // 
+            this.btnPrimero.Font = new System.Drawing.Font("Cambria", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPrimero.Location = new System.Drawing.Point(35, 73);
+            this.btnPrimero.Name = "btnPrimero";
+            this.btnPrimero.Size = new System.Drawing.Size(95, 34);
+            this.btnPrimero.TabIndex = 9;
+            this.btnPrimero.Text = "Primero";
+            this.btnPrimero.UseVisualStyleBackColor = true;
+            this.btnPrimero.Click += new System.EventHandler(this.btnPrimero_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Font = new System.Drawing.Font("Cambria", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEliminar.Location = new System.Drawing.Point(305, 21);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(132, 34);
+            this.btnEliminar.TabIndex = 8;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // btnInsertar
+            // 
+            this.btnInsertar.Font = new System.Drawing.Font("Cambria", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInsertar.Location = new System.Drawing.Point(72, 21);
+            this.btnInsertar.Name = "btnInsertar";
+            this.btnInsertar.Size = new System.Drawing.Size(132, 34);
+            this.btnInsertar.TabIndex = 7;
+            this.btnInsertar.Text = "Insertar";
+            this.btnInsertar.UseVisualStyleBackColor = true;
+            this.btnInsertar.Click += new System.EventHandler(this.btnInsertar_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Lime;
+            this.panel1.Controls.Add(this.txtCantidadHoras);
+            this.panel1.Controls.Add(this.txtNombreAsig);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(99, 82);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(445, 141);
+            this.panel1.TabIndex = 24;
+            // 
+            // txtCantidadHoras
+            // 
+            this.txtCantidadHoras.BackColor = System.Drawing.Color.White;
+            this.txtCantidadHoras.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCantidadHoras.Location = new System.Drawing.Point(258, 75);
+            this.txtCantidadHoras.Name = "txtCantidadHoras";
+            this.txtCantidadHoras.Size = new System.Drawing.Size(133, 26);
+            this.txtCantidadHoras.TabIndex = 5;
+            // 
+            // txtNombreAsig
+            // 
+            this.txtNombreAsig.BackColor = System.Drawing.Color.White;
+            this.txtNombreAsig.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNombreAsig.Location = new System.Drawing.Point(258, 35);
+            this.txtNombreAsig.Name = "txtNombreAsig";
+            this.txtNombreAsig.Size = new System.Drawing.Size(133, 26);
+            this.txtNombreAsig.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Cambria", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(26, 79);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(169, 22);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Cantidad de Horas";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Cambria", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(26, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(205, 22);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nombre de Asignatura";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Lime;
+            this.label1.Font = new System.Drawing.Font("Cooper Black", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(157, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(342, 27);
+            this.label1.TabIndex = 23;
+            this.label1.Text = "LISTA  DE  ASIGNATURAS";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(711, 450);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel2.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnUltimo;
+        private System.Windows.Forms.Button btnAnterior;
+        private System.Windows.Forms.Button btnSiguiente;
+        private System.Windows.Forms.Button btnPrimero;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnInsertar;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtCantidadHoras;
+        private System.Windows.Forms.TextBox txtNombreAsig;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ListaDblCircular/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ListaDblCircular
+{
+    public partial class Form1 : Form
+    {
+        TLisAsig Lista;
+        public Form1()
+        {
+            Lista = new TLisAsig();
+            InitializeComponent();
+        }
+
+        private void btnInsertar_Click(object sender, EventArgs e)
+        {
+            if (txtNombreAsig.Text == "" || txtCantidadHoras.Text == "")
+            {
+                MessageBox.Show("ESCRIBA LOS DATOS");
+                return;
+            }
+            else
+            {
+                Lista.crearLista(txtNombreAsig.Text, int.Parse(txtCantidadHoras.Text));
+                txtNombreAsig.Clear(); txtCantidadHoras.Clear();
+                txtNombreAsig.Focus();
+            }
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            if (Lista.vacia()) MessageBox.Show("Error: La lista esta VACIA");
+            if (txtNombreAsig.Text == "" || txtCantidadHoras.Text == "")
+                MessageBox.Show("No ha seleccionado ningun elemento");
+            else
+            {
+                Lista.eliminarLista(txtNombreAsig.Text);
+            }
+            txtNombreAsig.Clear();
+            txtCantidadHoras.Clear();
+        }
+
+        private void btnPrimero_Click(object sender, EventArgs e)
+        {
+            TNodoAsig nodoPrimero;
+            nodoPrimero = (TNodoAsig)Lista.getPrimero();
+            if (Lista.getPrimero() == null)
+            {
+                MessageBox.Show("LA LISTA ESTA VACIA");
+            }
+            else
+            {
+                Lista.getPrimero();
+                txtNombreAsig.Text = nodoPrimero.dameAsig();
+                txtCantidadHoras.Text = nodoPrimero.dameHoras().ToString();
+            }
+        }
+
+        private void btnUltimo_Click(object sender, EventArgs e)
+        {
+            TNodoAsig nodoUltimo;
+            nodoUltimo = (TNodoAsig)Lista.getUltimo();
+            if (Lista.getUltimo() == null)
+            {
+                MessageBox.Show("LA LISTA ESTA VACIA");
+            }
+            else
+            {
+                Lista.getUltimo();
+                txtNombreAsig.Text = nodoUltimo.dameAsig();
+                txtCantidadHoras.Text = nodoUltimo.dameHoras().ToString();
+            }
+        }
+
+        private void btnSiguiente_Click(object sender, EventArgs e)
+        {
+            if (Lista.BuscarAsig(txtNombreAsig.Text) == true)
+            {
+                TNodoAsig nodoSiguiente;
+                nodoSiguiente = (TNodoAsig)Lista.getProxCursor();
+                Lista.getProxCursor();
+                txtNombreAsig.Text = nodoSiguiente.dameAsig();
+                txtCantidadHoras.Text = nodoSiguiente.dameHoras().ToString();
+            }
+            else
+            {
+                MessageBox.Show("NO EXISTE SUCESOR");
+            }
+        }
+
+        private void btnAnterior_Click(object sender, EventArgs e)
+        {
+            if (Lista.BuscarAsig(txtNombreAsig.Text) == true)
+            {
+                TNodoAsig nodoAnterior;
+                nodoAnterior = (TNodoAsig)Lista.getAntCursor();
+                Lista.getAntCursor();
+                txtNombreAsig.Text = nodoAnterior.dameAsig();
+                txtCantidadHoras.Text = nodoAnterior.dameHoras().ToString();
+            }
+            else
+            {
+                MessageBox.Show("NO EXISTE ANTECESOR");
+            }
+        }
+    }
+}
